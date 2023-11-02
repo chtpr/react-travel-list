5f799b6abbbdabc0146910b78dfd4e684bc03dd0
chtpr <lee.christophe@northeastern.edu>
2023-11-02 15:27:26 -0700

    first commit

---
+++ src/components/Stats.js
@@ -0,0 +1,24 @@
+export default function Stats({ items }) {
+  // example of early return
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  // example of derived state
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `You have ${numItems} items from your list, and you have already packed ${numPacked} item (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
